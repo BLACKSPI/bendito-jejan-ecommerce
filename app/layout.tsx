0d73307ee088b29e9b4bfd2994f27be74111daed
Stanley Macharia <gitaustanley736@gmail.com>
2024-02-29 14:33:42 +0300

    Final

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,8 @@ import { ClerkProvider } from '@clerk/nextjs'
 import './globals.css'
 import { ModalProvider } from '@/providers/modal-provider'
 import { ToasterProvider } from '@/providers/toast-provider'
+import { ThemeProvider } from '@/providers/theme-provider'
+
 //import prismadb from '@/lib/prismadb'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -23,9 +25,12 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en">
         <body className={inter.className}>
-          <ToasterProvider/>
-          <ModalProvider/>
-          {children}
+          <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+            <ToasterProvider/>
+            <ModalProvider/>
+            {children}
+          </ThemeProvider>  
+          
         </body>
       </html>
     </ClerkProvider>
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -3,6 +3,8 @@ import { MainNav } from "@/components/main-nav";
 import StoreSwitcher from "@/components/store-switcher";
 import { redirect } from "next/navigation";
 import prismadb from "@/lib/prismadb";
+import { ModeToggle } from "@/components/theme-toggle";
+
 
 const Navbar = async () => {
     const { userId } = auth();
@@ -21,9 +23,9 @@ const Navbar = async () => {
         <div className="border-b">
             <div className="flex h-16 items-center px-4">
                 <StoreSwitcher items={stores} />
-                    
-                    <MainNav className="mx-6" />
+                <MainNav className="mx-6" />
                     <div className="ml-auto flex items-center space-x-4">
+                        <ModeToggle/>
                         <UserButton afterSignOutUrl="/" />
                     </div>
                 
